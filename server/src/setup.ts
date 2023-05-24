7a09cfbae80d47d311b833fa7dfdfbc0972948e6
Danielle <211907670@idf.il>
2023-05-24 14:30:03 +0300

    remove console.log

--- server/src/setup.ts
+++ server/src/setup.ts
@@ -1,7 +1,6 @@
 import AppDataSource from "../config/ormconfig";
 
 export const initializeDB = () =>{
-    console.log("HI Guy")
     AppDataSource.initialize()
   .then(() => {
     console.log('Connected to database');
