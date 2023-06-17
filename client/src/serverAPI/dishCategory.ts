8a3c2cda4f38a195b34d90c975ba0aaedfc4e519
Danielle <211907670@idf.il>
2023-06-17 17:15:23 +0300

    fix unauthorized bug

--- client/src/serverAPI/dishCategory.ts
+++ client/src/serverAPI/dishCategory.ts
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axiosInstance from "./axiosInstance"
 import { DishCategory } from '../models/DishCategory'
 
 export default class DishCategoryServer {
     static createDishCategory(newDishCategory: DishCategory) {
-        return axios.post("/dishCategory", { newDishCategory })
+        return axiosInstance.post("/dishCategory", { newDishCategory })
             .then(response => response)
             .catch(err => err.message)
     }
 
     static getDishCategory() {
-        return axios.get("/dishCategory")
+        return axiosInstance.get("/dishCategory")
             .then(response => response.data.dishCategory)
             .catch(err => err.message)
     }
