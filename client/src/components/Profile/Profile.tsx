a22b5849ab1fa6b87425d8ae35c53b58df896a02
DanaBg <danusha115@gmail.com>
2023-06-17 23:26:50 +0300

    change profile design

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Chip, CircularProgress, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Stack, Typography } from "@mui/material";
+import { Box, Chip, CircularProgress, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Stack, Typography, makeStyles } from "@mui/material";
 import MealCategoryServer from "../../serverAPI/mealCategory";
 import { MealCategory } from "../../models/MealCategory";
 import jwtDecode from "jwt-decode";
@@ -10,6 +10,19 @@ import UserPreferenceServer from "../../serverAPI/userPreference";
 import { UserPreferences } from "../../models/UserPreferences";
 import { Cuisine } from "../../models/Cuisine";
 import { DishCategory } from "../../models/DishCategory";
+import { styled } from '@mui/material/styles';
+const backgroungImage = require("./fastFoodBackground.jpg");
+
+const Content = styled('div')({
+    display: 'flex',
+    marginTop: '11vh',
+    height: '87vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    backgroundImage: `url(${backgroungImage})`,
+    backgroundSize: 'cover'
+});
 
 const Profile: React.FC = (): JSX.Element => {
     const currentUserId = jwtDecode<User>(localStorage.getItem('token')|| "").id;
@@ -44,66 +57,63 @@ const Profile: React.FC = (): JSX.Element => {
 
 
     return (
-        <Container sx={{ bgcolor: 'antiquewhite', height: '95vh', width: '1000vw', display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: "3em" }} >
-            <Box
-                component="img"
-                sx={{
-                height: 200,
-                width: 350,
-                margin: 5
-                }}
-                alt="The house from the offer."
-                src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
-            />
-            <Typography sx={{ fontSize: 30 }}>{`${userData.firstName} ${userData.lastName}`}</Typography>
-            <div style={{ display: 'flex', alignItems: 'center', marginTop: 10}}>
-                <Typography sx={{ fontSize: 15, fontFamily: "sans-serif", marginRight: 2}}> Favorite Cuisines </Typography>
-                <Stack direction="row" spacing={1}>
+        <Content>
+            <Container>
+                <Typography sx={{ fontSize: 40, marginBottom: 5 }}>{`${userData.firstName} ${userData.lastName}`}</Typography>
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: 10}}>
+                    <Typography sx={{ fontSize: 17, fontFamily: "sans-serif", marginRight: 2}}> Favorite Cuisines: </Typography>
                     {
-                        userPreferences?.cuisines?.map((cuisine: Cuisine) => 
-                            <Chip label={cuisine.description} color="primary" variant="outlined" />
+                        userPreferences?.cuisines && userPreferences?.cuisines.length > 0 ? 
+                        <Stack direction="row" spacing={1}>
+                            {
+                                userPreferences?.cuisines?.map((cuisine: Cuisine) => 
+                                    <Chip label={cuisine.description} color="primary" variant="outlined" />
+                                )
+                            }
+                        </Stack>
+                        : 
+                        <Typography sx={{ fontSize: 17, fontFamily: "sans-serif"}}>None </Typography>
+                    }
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: 10}}>
+                    <Typography sx={{ fontSize: 17, fontFamily: "sans-serif", marginRight: 2, marginBottom: 5, marginTop: 5}}>Allergies: </Typography>
+                    {
+                        userPreferences?.dishCategories && userPreferences?.dishCategories.length > 0 ? 
+                        <Stack direction="row" spacing={1}>
+                            {
+                                userPreferences?.dishCategories.map((dishCategory:DishCategory) => 
+                                    <Chip label={dishCategory.description} color="secondary" variant="outlined" />
+                            ) 
+                            }
+                        </Stack>
+                        : 
+                        <Typography sx={{ fontSize: 17, fontFamily: "sans-serif"}}>None </Typography>
+                    }
+                </div>
+                
+                <FormControl>
+                    {
+                        mealCategoriesData &&
+                        mealCategoriesData.map((mealCategory: MealCategory) => 
+                        <div key={mealCategory.id}>
+                            <FormLabel id="demo-row-radio-buttons-group-label">{mealCategory.description}</FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby="demo-row-radio-buttons-group-label"
+                                name="row-radio-buttons-group"
+                                defaultValue="no"
+                                value={userPreferences?.mealCategories?.find((userMealCategory: MealCategory) => 
+                                    userMealCategory.id === mealCategory.id) ? "yes" : "no"}
+                            >
+                                <FormControlLabel value="no" control={<Radio />} label="No" />
+                                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                            </RadioGroup>
+                        </div>
                         )
                     }
-                </Stack>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', marginTop: 10}}>
-                <Typography sx={{ fontSize: 15, fontFamily: "sans-serif", margin: 2}}>Allergies: </Typography>
-                {
-                    userPreferences?.dishCategories && userPreferences?.dishCategories.length > 0 ? 
-                    <Stack direction="row" spacing={1}>
-                        {
-                            userPreferences?.dishCategories.map((dishCategory:DishCategory) => 
-                                <Chip label={dishCategory.description} color="secondary" variant="outlined" />
-                        ) 
-                        }
-                    </Stack>
-                    : 
-                    <Typography sx={{ fontSize: 15, fontFamily: "sans-serif"}}>None </Typography>
-                }
-            </div>
-            
-            <FormControl>
-                {
-                    mealCategoriesData &&
-                    mealCategoriesData.map((mealCategory: MealCategory) => 
-                    <div key={mealCategory.id}>
-                        <FormLabel id="demo-row-radio-buttons-group-label">{mealCategory.description}</FormLabel>
-                        <RadioGroup
-                            row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
-                            defaultValue="no"
-                            value={userPreferences?.mealCategories?.find((userMealCategory: MealCategory) => 
-                                userMealCategory.id === mealCategory.id) ? "yes" : "no"}
-                        >
-                            <FormControlLabel value="no" control={<Radio />} label="No" />
-                            <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                        </RadioGroup>
-                    </div>
-                    )
-                }
-            </FormControl>
-        </Container>
+                </FormControl>
+            </Container>
+        </Content>
     )
 }
 
